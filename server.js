837bd4f3d906746df859cd2002a86bbdfddaaedc
valentramos <ignacio1994.ir@gmail.com>
2024-07-04 22:50:34 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+let sensorData = { temperature: 0, humidity: 0 };
+
+app.use(bodyParser.json());
+
+app.post('/sensor-data', (req, res) => {
+  const { temperature, humidity } = req.body;
+  sensorData = { temperature, humidity };
+  console.log(`Received data: Temperature = ${temperature}, Humidity = ${humidity}`);
+  res.sendStatus(200);
+});
+
+app.get('/sensor-data', (req, res) => {
+  res.json(sensorData);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
